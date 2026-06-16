ffd0bbe5bfd0964ad5575f864b6fde31970dedd4
thinkfashz <faubricioedms@gmail.com>
2026-06-15 21:05:36 -0400

    add landing demo builder anchor

--- src/fabrick/modules/landing-builder/page.tsx
+++ src/fabrick/modules/landing-builder/page.tsx
@@ -125,7 +125,9 @@ export default async function LandingBuilderModulePage({
         </div>
       )}
 
-      <CreateGeneratedPageForm initialValues={initialValues} />
+      <section id="landing-demo-builder" className="scroll-mt-24">
+        <CreateGeneratedPageForm initialValues={initialValues} />
+      </section>
 
       <Card>
         <CardHeader>
