ec6205863aead0b6efc1d5664b58cfc7ed7b0851
thinkfashz <faubricioedms@gmail.com>
2026-06-20 01:53:26 -0400

    add reusable ecommerce module entrypoint

---
+++ src/fabrick/modules/e-commerce/page.tsx
@@ -0,0 +1,7 @@
+import OmnifixStoreClient from "@/app/dashboard/e-commerce/omnifix-store-client";
+
+export function EcommerceModulePage() {
+  return <OmnifixStoreClient />;
+}
+
+export default EcommerceModulePage;
